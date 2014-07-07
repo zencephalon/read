0bb288efff433f1289e2da79d96e4f9f25e93c29
Matthew Bunday <zenceph@gmail.com>
2014-07-07 17:19:48 -0400

    Mocking out the books routes.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,4 @@
 class BooksController < ApplicationController
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Reader::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resource :books
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
