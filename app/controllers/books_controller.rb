e361d5484da6973f261eeb4547d5bd14f0eaf724
Matthew Bunday <zenceph@gmail.com>
2014-07-08 14:53:53 -0400

    Saving uploaded files.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,5 +6,12 @@ class BooksController < ApplicationController
   end
 
   def create
+    uploaded_io = params[:book]
+    filename = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
+    File.open(filename, 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    
+    redirect_to '/'
   end
 end
